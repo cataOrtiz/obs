ddc2a84217cf6207ea8529be08a2ddeadd7fb8cd
Cristian <clupin@My-ubuntu.(none)>
2014-12-15 02:42:15 -0300

    cambios

--- observatorio/static/js/main.js
+++ observatorio/static/js/main.js
@@ -5,3 +5,10 @@
 $(document).ready(function(){
 	$("#contenedorTwit").html("<div>prueba</div><div>prueba</div><div>prueba</div>");
 })
+
+$(document).ready(function(){
+	$("#boton").click(function () {	
+		alert($('input:radio[name=optionsRadios]:checked').val());
+		$("#formulario").submit();
+	});
+});
